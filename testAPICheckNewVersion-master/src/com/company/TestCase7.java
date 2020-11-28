5b6e75c587082eba08f5d245e6b778e66f369283
Honganh2910 <75011270+Honganh2910@users.noreply.github.com>
2020-11-28 19:20:07 +0700

    Update TestCase7.java

--- testAPICheckNewVersion-master/src/com/company/TestCase7.java
+++ testAPICheckNewVersion-master/src/com/company/TestCase7.java
@@ -11,10 +11,10 @@ public class TestCase7 {
         if (res.data[4].equals(0)) {
             if (((!res.code.equals("9995")))) throw new AssertionError();
             System.out.println("Bạn đã bị khoá tài khoản");
-        }
+        
         ResponseLogin ress = TestCaseLoginAPI.callAPI("0965223417", "308abcd910", "12rt");
         if (((!ress.code.equals("1000")))) throw new AssertionError();
         System.out.println("Finished");
         }
     }
-
+}
