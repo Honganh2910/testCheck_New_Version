0802bbb026841acd711d663ab7b1cd13ae3cf51b
Honganh2910 <75011270+Honganh2910@users.noreply.github.com>
2020-11-28 19:20:56 +0700

    Update Constant.java

--- testAPICheckNewVersion-master/src/com/company/Constant.java
+++ testAPICheckNewVersion-master/src/com/company/Constant.java
@@ -2,7 +2,7 @@ package com.company;
 
 
 class Constant {
-    public static String GET_UPDATE = "https://project-facebook-clone.herokuapp.com/it4788/user/change_passwword";
+    public static String CHECK_NEW_VERSION = "https://project-facebook-clone.herokuapp.com/it4788/user/check_new_version";
     public static String LOG_IN  = "https://project-facebook-clone.herokuapp.com/it4788/user/login";
 
 }
