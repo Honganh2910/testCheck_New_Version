09df099f129a2dee6c297285a5a1b4ca25d8fba9
Honganh2910 <75011270+Honganh2910@users.noreply.github.com>
2020-11-29 14:25:44 +0700

    Update TestCase2.java

--- testAPICheckNewVersion-master/src/com/company/TestCase2.java
+++ testAPICheckNewVersion-master/src/com/company/TestCase2.java
@@ -1,13 +1,12 @@
 package com.company;
 
 public class TestCase2 {
-    public static void UnitTest2() throws Exception{
-
-        String last_update ="9.0";
-        ResponseLogin res = TestCaseLoginAPI.callAPI("0965223417","308abcd910","12rt");
-        String token = res.token;
+    public static void UnitTest2() throws Exception {
+        ResponseLogin res = TestCaseLoginAPI.callAPI("0965223417", "308abcd910", "12rt");
+        String last_update = "9.0";
+        String token = res.data1.token;
         System.out.println("Unit test 2: Token is incorrect");
-         if (token.equals(" ")) {
+        if (token.equals(" ")) {
             ResponseCheckNewVersion ress = TestCaseCheckNewVersion.callAPI(last_update, token);
             if (!(ress.code.equals("1004"))) throw new AssertionError();
             if ((!"Parameter value is invalid".equals(ress.message))) throw new AssertionError();
