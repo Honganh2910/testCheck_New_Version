3fb3997a524d3a3f66f4276fdbeb4a88961ff3f2
Honganh2910 <75011270+Honganh2910@users.noreply.github.com>
2020-11-29 14:27:11 +0700

    Update TestCase4.java

--- testAPICheckNewVersion-master/src/com/company/TestCase4.java
+++ testAPICheckNewVersion-master/src/com/company/TestCase4.java
@@ -1,14 +1,12 @@
 package com.company;
 
-import org.json.JSONException;
 
-import java.io.IOException;
 
 public class TestCase4 {
     public static void UnitTest4() throws Exception {
         ResponseLogin ress = TestCaseLoginAPI.callAPI("0965223417", "308abcd910", "12rt");
         String last_update = "9.0";
-        String token = ress.token;
+        String token = ress.data1.token;
         System.out.println("Unit test 4: The account is blocked");
         ResponseCheckNewVersion res = TestCaseCheckNewVersion.callAPI(last_update, token);
         if (((!res.code.equals("9995")))) throw new AssertionError();
