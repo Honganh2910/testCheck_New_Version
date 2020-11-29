f36b6ae005f36add5eb3033a87858219a8fe3bf7
Honganh2910 <75011270+Honganh2910@users.noreply.github.com>
2020-11-29 14:26:09 +0700

    Update TestCase3.java

--- testAPICheckNewVersion-master/src/com/company/TestCase3.java
+++ testAPICheckNewVersion-master/src/com/company/TestCase3.java
@@ -6,7 +6,7 @@ public class TestCase3 {
     public static void UnitTest3() throws Exception {
         ResponseLogin resp = TestCaseLoginAPI.callAPI("0965223417","308abcd910","12rt");
         String last_update ="9.0";
-        String token = resp.token;
+        String token = resp.data1.token;
         System.out.println("Unit test 3: Cant't find data you are looking for");
         ResponseCheckNewVersion res = TestCaseCheckNewVersion.callAPI(last_update,token);
         if (!(res.code.equals("1001"))) throw new AssertionError();
