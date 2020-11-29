80b93cd98e2eb7407b9348cc5be0d3c77af3accb
Honganh2910 <75011270+Honganh2910@users.noreply.github.com>
2020-11-29 14:29:10 +0700

    Update TestCaseCheckNewVersion.java

--- testAPICheckNewVersion-master/src/com/company/TestCaseCheckNewVersion.java
+++ testAPICheckNewVersion-master/src/com/company/TestCaseCheckNewVersion.java
@@ -2,19 +2,22 @@ package com.company;
 
 import com.google.gson.Gson;
 import org.json.*;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 
+import com.google.gson.Gson;
 import com.company.ResponseCheckNewVersion;
 
 public class TestCaseCheckNewVersion {
     public static ResponseCheckNewVersion callAPI( String last_update ,String token) throws IOException, JSONException {
-        URL ur = new URL(Constant.GET_UPDATE +
+        URL ur = new URL(Constant.CHECK_NEW_VERSION +
                             "?lastupdate="+ last_update +"&token=" +token);
         HttpURLConnection conn = (HttpURLConnection) ur.openConnection();
         conn.setRequestMethod("POST");
@@ -33,18 +36,7 @@ public class TestCaseCheckNewVersion {
             }
             Gson g = new Gson();
             System.out.println(content.toString());
-            JSONObject objectJson = new JSONObject(content.toString());
-            String version = objectJson.getJSONObject("data").getString("version");
-            String require = objectJson.getJSONObject("data").getString("require");
-            String url = objectJson.getJSONObject("data").getString("url");
-            String user_id = objectJson.getJSONObject("data").getString("user_id");
-            String active = objectJson.getJSONObject("data").getString("active");
             ResponseCheckNewVersion t =  g.fromJson(content.toString(), ResponseCheckNewVersion.class);
-            t.data[0] = version;
-            t.data[1] = require;
-            t.data[2] = url;
-            t.data[3] = user_id;
-            t.data[4] = active;
             return t;
         } finally {
             conn.disconnect();
